07f2d487c86cbe2d3f172cc76ca8500a13a66218
Augit <testingaugit@gmail.com>
2019-02-08 17:57:06 +0800

    login api validation

--- app/src/main/java/com/example/crowdzrnew/feature/login/LoginActivity.kt
+++ app/src/main/java/com/example/crowdzrnew/feature/login/LoginActivity.kt
@@ -2,11 +2,16 @@ package com.example.crowdzrnew.feature.login
 
 
 import android.databinding.DataBindingUtil
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.res.ResourcesCompat
 import android.support.v7.widget.AppCompatEditText
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.KeyEvent
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.widget.EditText
+import android.widget.TextView
 import com.example.crowdzrnew.AppPreference
 import com.example.crowdzrnew.R
 import com.example.crowdzrnew.core.BaseActivity
@@ -15,6 +20,7 @@ import com.example.crowdzrnew.core.event.SnackbarMessage
 import com.example.crowdzrnew.core.event.StartActivityEvent
 import com.example.crowdzrnew.core.event.StartActivityModel
 import com.example.crowdzrnew.databinding.ActivityLoginBinding
+import com.example.crowdzrnew.utilities.KeyboardManager.Companion.hideKeyboard
 import com.example.crowdzrnew.utilities.observe
 import com.example.crowdzrnew.utilities.showSnackbar
 import dagger.android.AndroidInjection
@@ -37,6 +43,7 @@ class LoginActivity : BaseActivity(),LoginViewModel.UserLoginListener{
         binding.setLifecycleOwner(this)
         setupEvents()
         setupIntents()
+        setupActions()
     }
 
     private fun setupEvents(){
@@ -78,7 +85,72 @@ class LoginActivity : BaseActivity(),LoginViewModel.UserLoginListener{
         }
     }
 
+    private fun setupActions() {
+        // et_password.getEditText().setOnEditorActionListener(TextView.OnEditorActionListener(this::onKeyboardAction))
 
+        et_email.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                checkIsEmpty(et_email)
+                if (s != null) {
+                    viewModel.username.set(s.toString())
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                checkIsEmpty(et_email)
+            }
+
+        })
+        et_password.setOnEditorActionListener(TextView.OnEditorActionListener(this::onKeyboardAction))
+
+        checkIsEmpty(et_password)
+        et_password.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable?) {
+                checkIsEmpty(et_password)
+                if (s != null) {
+                    viewModel.password.set(s.toString())
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                checkIsEmpty(et_password)
+            }
+
+        })
+    }
+
+
+    private fun checkIsEmpty(view: EditText) {
+        if (view.text!!.isEmpty()) {
+            view.typeface = ResourcesCompat.getFont(this@LoginActivity, R.font.open_sans_italic)
+        } else {
+            view.typeface = ResourcesCompat.getFont(this@LoginActivity, R.font.open_sans_bold)
+        }
+    }
+
+    private fun onKeyboardAction(view: TextView, actionId: Int, keyEvent: KeyEvent?): Boolean {
+        if (actionId == EditorInfo.IME_ACTION_DONE || actionId == EditorInfo.IME_NULL && keyEvent?.action == KeyEvent.ACTION_DOWN) {
+            onDone(et_password)
+
+        }
+        return true
+    }
+
+    fun onDone(view: EditText) {
+        hideKeyboard(this)
+        if (viewModel.isFormValid.get()) {
+            viewModel.onLoginClicked()
+        }
+    }
     override fun onFieldError(id: Int, msg: Int) {
         val view = findViewById<View>(id)
         when (view) {
